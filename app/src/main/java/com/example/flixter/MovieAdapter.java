23f7f0f4a933a77152a136009e9839425e9c3ab5
pjrodz4 <pjrodz4@gmail.com>
2019-06-28 11:35:43 -0700

    Implemented required feautures.

---
+++ app/src/main/java/com/example/flixter/MovieAdapter.java
@@ -0,0 +1,116 @@
+package com.example.flixter;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixter.models.Config;
+import com.example.flixter.models.Movie;
+
+import java.util.ArrayList;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    // list of movies
+    ArrayList<Movie> movies;
+    // config need for image urls
+    Config config;
+    // context for rendering
+    Context context;
+
+    // initialize with list
+    public MovieAdapter(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public Config getConfig() {
+        return config;
+    }
+
+    public void setConfig(Config config) {
+        this.config = config;
+    }
+
+    // creates and inflates a new view
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        // get the context and create the inflater
+        context = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        // create the view using the item_movie layout
+        View movieView = inflater.inflate(R.layout.item_movie, viewGroup, false);
+        // return a new ViewHolder
+        return new ViewHolder(movieView);
+    }
+
+    // binds an inflated view to a new item
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        // get the movie data at the specified position
+        Movie movie = movies.get(i);
+        // populate the view with the movie data
+        viewHolder.tvTitle.setText(movie.getTitle());
+        viewHolder.tvOverview.setText(movie.getOverview());
+
+        // determine the current orientation
+        boolean isPortrait = context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+
+        // build url for poster image
+        String imageUrl = null;
+
+        // if in portrait mode, load poster image
+        if (isPortrait) {
+            imageUrl = config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+        } else {
+            imageUrl = config.getImageUrl(config.getBackdropSize(), movie.getBackdropPath());
+        }
+
+        // get the correct placeholder and imageView for the current orientation
+        int placeHolderID = isPortrait ? R.drawable.flicks_movie_placeholder : R.drawable.flicks_backdrop_placeholder;
+        ImageView imageView = isPortrait ? viewHolder.ivPosterImage : viewHolder.ivBackdropImage;
+
+        // load image using glide
+        Glide.with(context)
+                .load(imageUrl)
+                .bitmapTransform(new RoundedCornersTransformation(context, 25, 0))
+                .placeholder(placeHolderID)
+                .error(placeHolderID)
+                .into(imageView);
+    }
+
+    // returns the total number of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    // create the viewholder as a static inner class
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        // track view objects
+        ImageView ivPosterImage;
+        ImageView ivBackdropImage;
+        TextView tvTitle;
+        TextView tvOverview;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // lookup view objects by id
+            ivPosterImage = (ImageView) itemView.findViewById(R.id.ivPosterImage);
+            ivBackdropImage = (ImageView) itemView.findViewById(R.id.ivBackdropImage);
+            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+            tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
+
+        }
+    }
+}
